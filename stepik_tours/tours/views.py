706afa14ee0bf89fe867abfeae0e4ee5d96053cb
azabirov <amirzabirov@gmail.com>
2021-03-20 23:35:51 +0400

    DjangoProjectV2

--- stepik_tours/tours/views.py
+++ stepik_tours/tours/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+import random
 # from django.views import View
 from django.http import HttpResponseNotFound, HttpResponseServerError
+from django.shortcuts import render
 # Create your views here.
 from tours import data
-import random
 
 
 def main_view(request):
@@ -15,14 +15,15 @@ def main_view(request):
     for tour_id in range(6):
         while True:
             tour_id = random.randint(1, len(data.tours))
-            if tour_id not in tour_id_list: break
+            if tour_id not in tour_id_list:
+                break
         tours_element = data.tours[tour_id]
         tours_element['tourid'] = tour_id
         tours.append(tours_element)
         tour_id_list.append(tour_id)
-    return render(request, "tours/index.html", context = {
-        'tours':tours,
-        'image_random':image_random,
+    return render(request, "tours/index.html", context={
+        'tours': tours,
+        'image_random': image_random,
     })
 
 
@@ -32,17 +33,25 @@ def departure_view(request, departure):
         if data.tours[number]['departure'] == departure:
             tours.append(data.tours[number])
             data.tours[number]['pricetofit'] = '{:,}'.format(data.tours[number]['price']).replace(',', ' ')
-        minprice = '{:,}'.format(min([int(data.tours[tour]['price']) for tour in data.tours if data.tours[tour]['departure'] == departure])).replace(',', ' ')
-        maxprice = '{:,}'.format(max([int(data.tours[tour]['price']) for tour in data.tours if data.tours[tour]['departure'] == departure])).replace(',', ' ')
-        minnights = '{:,}'.format(min([int(data.tours[tour]['nights']) for tour in data.tours if data.tours[tour]['departure'] == departure])).replace(',', ' ')
-        maxnights = '{:,}'.format(max([int(data.tours[tour]['nights']) for tour in data.tours if data.tours[tour]['departure'] == departure])).replace(',', ' ')
-    return render(request, "tours/departure.html", context = {
-        'departure':departure,
-        'tours':tours,
-        'minprice':minprice,
-        'maxprice':maxprice,
-        'minnights':minnights,
-        'maxnights':maxnights,
+        minprice = '{: ,}'.format(min([int(data.tours[tour]['price'])
+                                       for tour in data.tours if data.tours[tour]['departure'] == departure]))\
+            .replace(',', ' ')
+        maxprice = '{: ,}'.format(max([int(data.tours[tour]['price'])
+                                       for tour in data.tours if data.tours[tour]['departure'] == departure]))\
+            .replace(',', ' ')
+        minnights = '{: ,}'.format(min([int(data.tours[tour]['nights'])
+                                        for tour in data.tours if data.tours[tour]['departure'] == departure]))\
+            .replace(',', ' ')
+        maxnights = '{: ,}'.format(max([int(data.tours[tour]['nights'])
+                                        for tour in data.tours if data.tours[tour]['departure'] == departure]))\
+            .replace(',', ' ')
+    return render(request, "tours/departure.html", context={
+        'departure': departure,
+        'tours': tours,
+        'minprice': minprice,
+        'maxprice': maxprice,
+        'minnights': minnights,
+        'maxnights': maxnights,
     })
 
 
@@ -50,8 +59,8 @@ def tour_view(request, tour_id):
     tours = data.tours[tour_id]
     tours['hotelstars'] = int(tours['stars']) * '★'
     tours['pricetofit'] = '{:,}'.format(tours['price']).replace(',', ' ')
-    return render(request, "tours/tour.html", context = {
-        'tours':tours,
+    return render(request, "tours/tour.html", context={
+        'tours': tours,
     })
 
 
